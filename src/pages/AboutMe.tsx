080b5c27b096dc6cfbbc54a399c5fbac355b1477
Lucia Kokesova <lkokesova@gmail.com>
2023-08-30 00:01:22 +0200

    add contact form

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = (props: Props) => {
     <>
       <h2 className="text-xl font-semibold mb-2 ">About me</h2>
       <div className="max-w-screen-md text-gray-600">
-        <p className="mt-2">
+        <p className="mt-2 indent-6">
           I am a software developer with experience in building web
           applications. My passion is to create beautiful, responsive,
           user-friendly websites. I'm always eager to learn new things. I have
@@ -17,12 +17,12 @@ const AboutMe = (props: Props) => {
           version control systems like Git and GitHub.
         </p>
 
-        <p className="mt-4">
+        <p className="mt-4 indent-6">
           My main focus has been developing front end components using ReactJs
           and TypeScript. I use Tailwind CSS for styling my projects.
         </p>
 
-        <p className="mt-4">
+        <p className="mt-4 indent-6">
           I love working on challenging projects that require creativity and
           problem solving. I'm currently looking for opportunities where I can
           utilize my knowledge of programming languages, frameworks, and
@@ -32,7 +32,7 @@ const AboutMe = (props: Props) => {
           goal is to become an expert in this field.
         </p>
 
-        <p className="mt-4">
+        <p className="mt-4 indent-6">
           In my free time I enjoy playing video games or watching movies. When
           not coding you will find me hiking, gardening, cooking or watching
           football on TV. I also like playing boardgames.
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,13 +11,81 @@ const Contact = (props: Props) => {
         following channels:
       </p>
       <div className="mt-2 text-gray-600">
-        <p>Email: lkokesova@gmail.com</p>
         <p>
-          LinkedIn:{" "}
-          <a href="https://www.linkedin.com/in/lucia-kokesova/">
+          <i className="fa-solid fa-envelope"></i> lkokesova@gmail.com
+        </p>
+        <p>
+          <i className="fab fa-linkedin mr-1"></i>
+          <a
+            href="https://www.linkedin.com/in/lucia-kokesova/"
+            className="underline hover:no-underline hover:text-indigo-600"
+          >
             Lucia Kokešová
           </a>
         </p>
+        <p>
+          <i className="fa-brands fa-github"></i>
+          <a
+            href="https://github.com/lkokes"
+            className="underline hover:no-underline hover:text-indigo-600"
+          >
+            lkokes
+          </a>
+        </p>
+      </div>
+      <div className="w-1/2 border rounded-lg px-6 py-5 mt-3">
+        <h3 className="text-xl font-semibold mt-3 mb-2">Contact me</h3>
+        <form className="mt-2">
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email <span className="text-red-500">*</span>
+            </label>
+            <input
+              required
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Send
+          </button>
+        </form>
       </div>
     </>
   );
